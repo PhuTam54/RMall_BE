7bdae1cf50dfde91c38be840ca687fad1b7bb6a9
PhuTam54 <phutamytb@gmail.com>
2024-03-18 10:41:15 +0700

    Init BE project structure

---
+++ DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RMall_BE
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Todo> Todo => Set<Todo>();
+    }
+}
