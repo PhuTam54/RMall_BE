5c2e4ece5ac005d7b6febdfd0e03981252fdb6d9
PhuTam54 <phutamytb@gmail.com>
2024-04-14 15:00:27 +0700

    Update Order: let Customer update status

--- Repositories/OrderRepositories/OrderRepository.cs
+++ Repositories/OrderRepositories/OrderRepository.cs
@@ -33,19 +33,20 @@ namespace RMall_BE.Repositories.OrderRepositories
         public ICollection<Order> GetAllOrder()
         {
             var orders = _context.Orders
-                .Include(o => o.OrderFoods)
-                    .ThenInclude(of => of.Food)
-                .Include(o => o.Tickets)
-                    .ThenInclude(t => t.Seat)
-                        .ThenInclude(s => s.SeatType)
-                            .ThenInclude(st => st.SeatPricings)
                 .ToList();
             return orders;
         }
 
         public Order GetOrderById(int id)
         {
-            return _context.Orders.FirstOrDefault(o => o.Id == id);
+            return _context.Orders
+                .Include(o => o.OrderFoods)
+                    .ThenInclude(of => of.Food)
+                .Include(o => o.Tickets)
+                    .ThenInclude(t => t.Seat)
+                        .ThenInclude(s => s.SeatType)
+                            .ThenInclude(st => st.SeatPricings)
+                .FirstOrDefault(o => o.Id == id);
         }
 
         public Order GetOrderByOrderCode(string orderCode)
@@ -63,7 +64,14 @@ namespace RMall_BE.Repositories.OrderRepositories
 
         public ICollection<Order> GetOrderByUserId(int userId)
         {
-            return _context.Orders.Where(o => o.User_Id == userId).ToList();
+            return _context.Orders
+                .Include(o => o.OrderFoods)
+                    .ThenInclude(of => of.Food)
+                .Include(o => o.Tickets)
+                    .ThenInclude(t => t.Seat)
+                        .ThenInclude(s => s.SeatType)
+                            .ThenInclude(st => st.SeatPricings)
+                .Where(o => o.User_Id == userId).ToList();
         }
 
         public bool CreateOrderFood(OrderFood orderFood)
